0c23ba359e5f3e50671dbe932884cfd1e70692e7
Andrew Clarke <andreweskeclarke@gmail.com>
2017-12-02 15:38:28 +0000

    Move things around

--- experiments.sh
+++ experiments.sh
@@ -7,7 +7,14 @@ echo "Running tests first to ensure nothing is broken..."
 bash run_test.sh
 
 echo "Running UCB experiments with single bandit per episode..."
-for f in ucb_single_bandit_experiments/[^_]*.py; do 
+for f in experiments/ucb_single_bandit_experiments/[^_]*.py; do 
+    echo ""
+	echo -n "Running experiment defined in: $f"
+	time PYTHONPATH=`pwd` python "$f"
+done
+
+echo "Running UCB experiments with two bandits per episode..."
+for f in experiments/ucb_two_bandit_experiments/[^_]*.py; do 
     echo ""
 	echo -n "Running experiment defined in: $f"
 	time PYTHONPATH=`pwd` python "$f"
--- generate_visualizations.sh
+++ generate_visualizations.sh
@@ -6,6 +6,9 @@ source activate bandits
 echo "Running tests first to ensure nothing is broken..."
 bash run_test.sh
 
-echo "Running UCB visualizations..."
-DISPLAY=:0 PYTHONPATH=`pwd` python bandits/experiment_results_visualizer.py ucb_single_bandit_experiments/*/results.json
+echo "Generating visualizations for UCB agent with single bandit environment..."
+DISPLAY=:0 PYTHONPATH=`pwd` python bandits/experiment_results_visualizer.py experiments/ucb_single_bandit_experiments/*/results.json
+
+echo "Generating visualizations for UCB agent with two bandit enviroment..."
+DISPLAY=:0 PYTHONPATH=`pwd` python bandits/experiment_results_visualizer.py experiments/ucb_two_bandit_experiments/*/results.json
 
